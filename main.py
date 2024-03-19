800d41467e189e0fe4b3d4102a688a3af65ea21b
erudmila <144694341+erudmila@users.noreply.github.com>
2024-03-19 12:58:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+print("Let's play Silly Sentences!")
+
+name=input("Enter a name: ")
+adjectiveone=input("Enter an adjective: ")
+adjectivetwo=input("Enter an adjective: ")
+adverb=input("Enter a adverb: ")
+foodone=input("Enter a food: ")
+foodtwo=input("Enter another food: ")
+noun=input("Enter a noun: ")
+place=input("Enter a place: ")
+verb=input("Enter a verb: ")
+
+print()
+
+print(name + " was planning a dream vacation to " + place + ".")
+print(name + " was especially looking forward to trying the local")
+print("cuisine, including " + adjectiveone + " " + foodone + " and "+ foodtwo + ".")
+
+print()
+
+print(name + " will have to practice the language " + adverb + " to" )
+print("make it easier to " + verb + " with people.")
+
+print()
+
+print(name + " has a long list of sights to see, including the")
+print(noun + " museum and the " + adjectivetwo + " park.")
